6275833be5338a03d0233749b8e807a28ac897a3
surenderpal <surenderpal134@gmail.com>
2020-09-02 21:57:00 +0530

    automated GDrive

---
+++ gmail.py
@@ -0,0 +1,33 @@
+# from selenium import webdriver
+# import time
+
+# driver=webdriver.Chrome()
+# driver.get("https://www.gmail.com/")
+# def login():#username,password
+#     # driver.find_element
+#     driver.find_element_by_id("identifierId").send_keys("surenderpal134@gmail.com")
+#     driver.find_element_by_xpath("//span[@jsname='V67aGc']").click()
+#     pass
+
+
+# login()
+from selenium import webdriver
+from time import sleep
+
+class Google:
+
+ def __init__(self):
+  self.driver=webdriver.Chrome()
+  self.driver.get('https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent%27')
+  sleep(3)
+  self.driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
+  self.driver.find_element_by_xpath('//input[@type="email"]').send_keys('surenderpal134@gmail.com')
+  self.driver.find_element_by_xpath('//*[@id="identifierNext"]').click()
+  sleep(3)
+  self.driver.find_element_by_xpath('//input[@type="password"]').send_keys('SUR@2000')
+  self.driver.find_element_by_xpath('//*[@id="passwordNext"]').click()
+  sleep(2)
+  self.driver.get('https://drive.google.com/drive/folders/1GdsRlvo2T-DUzP-RDtBYRkAyhSZi2jPH')
+  sleep(5)
+
+mylike= Google()
